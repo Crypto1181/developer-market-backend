ac4ceb7ffdc53e8d88cf59c727510e7fd7bac851
Crypto1181 <cryptopluto841@gmail.com>
2025-10-29 08:25:49 -0400

    Initial commit - Strapi Backend

---
+++ src/api/payment-settings/controllers/payment-settings.ts
@@ -0,0 +1,7 @@
+/**
+ * payment-settings controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::payment-settings.payment-settings');
---
+++ src/api/payment-settings/services/payment-settings.ts
@@ -0,0 +1,7 @@
+/**
+ * payment-settings service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::payment-settings.payment-settings');
